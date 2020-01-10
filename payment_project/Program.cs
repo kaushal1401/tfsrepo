200da48fb3381c1a74e06165cebfec92fb677591
singhk <CORP\singhk>
2020-01-10 04:05:55 +0000

    git-tfs-id: [http://desktop-rd5g1bc/test]$/TFS2020/Hello-World;C46

---
+++ payment_project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace payment_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Wordl!");
+        }
+    }
+}
